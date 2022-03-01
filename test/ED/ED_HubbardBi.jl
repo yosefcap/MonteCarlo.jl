f86e1a991283d868461b8641460d90b633c820b9
yosefcap <yosefcaplan@gmail.com>
2022-03-01 17:10:33 +0200

    Update ED_HubbardBi.jl

--- test/ED/ED_HubbardBi.jl
+++ test/ED/ED_HubbardBi.jl
@@ -153,22 +153,24 @@ function observables(spacial_dims::NTuple{DIMS,Int64},num_species::Int64, t::Flo
     ,βs::Vector{Float64},taus::Vector{Float64}) where {DIMS}
    
     if length(spacial_dims)==2 && spacial_dims[2]==1
-        spacial_dims=(spacial_dims[1],) 
+        spacial_dims=spacial_dims=(spacial_dims[1],) #(spacial_dims[1]) #
     end
     num_spin=2
     N_max=prod(spacial_dims)
     nt=0:N_max
     n=[]
     for i in 1:num_spin*num_species
+
         push!(n,nt)
     end
     
     Nₙs=collect(Iterators.product(n...))[:]
+    
     b=length(βs)
     tt=length(taus)
     partitions = zeros(Float64,b)
     energys = zeros(Float64,b)
-    numbers = zeros(Float64,b)
+    numbers = zeros(Float64,b) 
     SC_corrs=zeros(Float64,spacial_dims...,b)
     sw_tau=zeros(Float64,spacial_dims...,tt,b)
     dw_tau=zeros(Float64,spacial_dims...,2,2,tt,b)
@@ -177,8 +179,13 @@ function observables(spacial_dims::NTuple{DIMS,Int64},num_species::Int64, t::Flo
     for Nₙ in Nₙs 
         for i in 1:b #runs over βs
             β=βs[i] 
-            if !isempty(taus)
-                Nₘ =  (Nₙ[1]+1,Nₙ[2],Nₙ[3]+1,Nₙ[4])
+            if !isempty(taus)                
+                Nₘ =  zeros(Int64,length(Nₙ))
+                for i in 1:length(Nₘ)
+                     mod(i,num_species)==0 ? Nₘ[i]=Nₙ[i]+1 : Nₘ[i]=Nₙ[i]
+                end
+                Nₘ = (Nₘ...,)
+                #(Nₙ[1]+1,Nₙ[2],Nₙ[3]+1,Nₙ[4])
                 Dₘ =[]
                 Uₘ=[]
                 if Nₘ in Nₙs
@@ -191,30 +198,32 @@ function observables(spacial_dims::NTuple{DIMS,Int64},num_species::Int64, t::Flo
             partitions[i] += partition_obs(Dₙ ,β)
             energys[i]    += energy_obs(Dₙ ,β)
             numbers[i]    += number_obs(Dₙ , Nₙ, β)
-            for   xt in 1:spacial_dims[1]
-                for yt  in 1:spacial_dims[2]
-                    x=1
-                    y=1
-                    SC_corrs[xt,yt,i]+=SC_corr_obs(spacial_dims,CartesianIndex(x,y) , CartesianIndex(xt,yt) , Nₙ , Dₙ , Uₙ, β)
-                    if !isempty(taus) && !isempty(Dₘ)
-                        sw_tau[xt,yt,:,i]+=SC_corr_sw_tau_obs(spacial_dims,num_species,CartesianIndex(x,y),CartesianIndex(xt,yt) 
+            ds=findall(ones(spacial_dims).!=0)
+          #  for   xt in 1:spacial_dims[1]
+            #    for yt  in 1:spacial_dims[2]
+            for r in ds
+                r0=ds[1]
+                SC_corrs[r,i]+=SC_corr_obs(spacial_dims,CartesianIndex(r0) , CartesianIndex(r) , Nₙ , Dₙ , Uₙ, β)
+                if !isempty(taus) && !isempty(Dₘ)
+                    sw_tau[r,:,i]+=SC_corr_sw_tau_obs(spacial_dims,num_species,CartesianIndex(r0),CartesianIndex(r) 
+                    , Nₙ , Dₙ  , Uₙ , Nₘ , Dₘ  , Uₘ , β,taus ) 
+                    if length(spacial_dims)>1
+                        dw_tau[r,:,:,:,i]+=SC_corr_dw_tau_obs(spacial_dims,num_species,CartesianIndex(r0),CartesianIndex(r)
                         , Nₙ , Dₙ  , Uₙ , Nₘ , Dₘ  , Uₘ , β,taus ) 
-                        if length(spacial_dims)>1
-                            dw_tau[xt,yt,:,:,:,i]+=SC_corr_dw_tau_obs(spacial_dims,num_species,CartesianIndex(x,y),CartesianIndex(xt,yt) 
-                            , Nₙ , Dₙ  , Uₙ , Nₘ , Dₘ  , Uₘ , β,taus ) 
-                        end
                     end
                 end
-            end           
+            end
+               # end
+           # end           
         end
     end
 
     for i in 1:b
         SC_corrs[:,:,i] = SC_corrs[:,:,i]./partitions[i]
         sw_tau[:,:,:,i] = sw_tau[:,:,:,i]./partitions[i]
-        dw_tau[:,:,:,i] = dw_tau[:,:,:,i]./partitions[i]
+        dw_tau[:,:,:,:,:,i] = dw_tau[:,:,:,:,:,i]./partitions[i]
     end
-    return partitions, energys./partitions, numbers./partitions,SC_corrs, sw_tau, dw_tau
+    return partitions, energys./partitions, numbers./partitions,num_species*SC_corrs, num_species*sw_tau, num_species*dw_tau
 end
 
 
@@ -308,10 +317,11 @@ function hamiltonian_operator(state::Array{Int8,DIMS2} ,  t::Float64,U::Float64,
     
     dims = size(state)
     spacial_dims = length(dims)-2
+    num_species = dims[spacial_dims+1]
     state_sp = Array{Int8,length(dims)}[]
     co=Float64[]
 
-    diag_term = U*sum( sum(state[ntuple(k->:,spacial_dims+1)...,1].-state[ntuple(k->:,spacial_dims+1)...,2] ,dims=spacial_dims+1 ).^2 ) #interaction term
+    diag_term = U*sum( sum(state[ntuple(k->:,spacial_dims+1)...,1].-state[ntuple(k->:,spacial_dims+1)...,2] ,dims=spacial_dims+1 ).^2 )/2/num_species #interaction term
     diag_term = -diag_term - μ*sum(state) #chemical potential term
     push!(state_sp,state)
     push!(co,diag_term)
@@ -324,7 +334,7 @@ function hamiltonian_operator(state::Array{Int8,DIMS2} ,  t::Float64,U::Float64,
                 index_i = occupation
                 index_j = hop(index_i,dir,lr,dims)
                 state_f , co_temp = hopping_operatr(state,index_i,index_j)
-                if co_temp != 0
+                if co_temp != 0 && state_f ∉ state_sp
                     push!(state_sp,state_f)
                     push!(co,-t*co_temp)
                 end
